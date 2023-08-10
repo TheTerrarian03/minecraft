cd4bac6ab729201229e4acd68159dc6acf3fb9b8
Logan Meyers <theterrarian03@gmail.com>
2023-08-10 15:43:08 -0700

    Update ui_interface.py

--- CUSTOM_SCRIPTS/ui_interface.py
+++ CUSTOM_SCRIPTS/ui_interface.py
@@ -6,9 +6,15 @@ class ComplexInterface:
         self.root = root
         self.root.title("Complex Tkinter Interface")
         
+        # Top Frame
+        top_frame = tk.Frame(root)
+        top_frame.pack(fill="both", expand=True)
+        
         # Left Side
-        left_frame = tk.Frame(root)
-        left_frame.pack(side="left", padx=10, pady=10)
+        left_frame = tk.Frame(top_frame)
+        left_frame.pack(side="left", padx=10, pady=10, fill="y")
+        
+        tk.Label(left_frame, text="Program Settings", font=("Helvetica", 16, "bold")).pack(anchor="w")
         
         self.disable_wifi_var = tk.BooleanVar()
         self.change_name_var = tk.BooleanVar()
@@ -27,38 +33,46 @@ class ComplexInterface:
         ttk.Combobox(left_frame, textvariable=self.profile_var, values=profiles).pack(fill="x")
         
         # Right Side
-        right_frame = tk.Frame(root)
-        right_frame.pack(side="right", padx=10, pady=10)
+        right_frame = tk.Frame(top_frame)
+        right_frame.pack(side="right", padx=10, pady=10, fill="both", expand=True)
         
-        self.groups = []
+        tk.Label(right_frame, text="Minecraft Settings", font=("Helvetica", 16, "bold")).pack(anchor="w")
+        
+        self.group_boxes = []  # (key box, value box)
+        self.input_frame = tk.Frame(right_frame)  # frame to hold all groups
         self.add_group()
+
+        plus_button = tk.Button(right_frame, text="+", command=self.add_group)
+        minus_button = tk.Button(right_frame, text="-", command=self.remove_group)
         
-        tk.Button(right_frame, text="+", command=self.add_group).pack()
-        tk.Button(right_frame, text="-", command=self.remove_group).pack()
+        plus_button.pack(side="left")
+        minus_button.pack(side="right")
         
-        # Bottom
+        # Bottom Frame
         bottom_frame = tk.Frame(root)
         bottom_frame.pack(fill="x", pady=20)
         
-        tk.Button(bottom_frame, text="WRITE!", command=self.write_settings).pack()
+        tk.Button(bottom_frame, text="Load Profile").pack(side="left", padx=10)
+        tk.Button(bottom_frame, text="WRITE!", command=self.write_settings).pack(side="right", padx=10)
 
     def add_group(self):
-        group_frame = tk.Frame(self.root)
-        group_frame.pack(side="top", padx=10, pady=5, fill="x")
+        group_frame = tk.Frame(self.groups_frame)  # add frame inside bigger frame
         
         key_entry = tk.Entry(group_frame)
         value_entry = tk.Entry(group_frame)
         
+        group_frame.pack(padx=10, pady=5, fill="x")
         key_entry.pack(side="left", padx=5)
         value_entry.pack(side="right", padx=5)
         
-        self.groups.append((key_entry, value_entry))
-        
+        self.group_boxes.append((key_entry, value_entry))
+
     def remove_group(self):
-        if self.groups:
-            group_frame = self.groups.pop()
-            group_frame[0].destroy()
-            group_frame[1].destroy()
+        # removes last group from list
+        if len(self.groups) > 0:
+            key_entry, value_entry = self.groups[-1]
+            key_entry.pack_forget()
+            value_entry.pack_forget()
 
     def write_settings(self):
         # Here you can implement the functionality to write the settings based on user input.
